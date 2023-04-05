137bb1aa90890b193aa57ecfbe7538c943ef12dd
M1saka2003 <3296582213@qq.com>
2023-04-05 22:05:41 +0800

    lambda

--- Modern C++/1.lambda捕获(C++11).cpp
+++ Modern C++/1.lambda捕获(C++11).cpp
@@ -17,9 +17,9 @@ int main()
     //auto foo1 = [x, y] { x++, y++ }; 编译器报错,因为是值捕获不允许修改
 
     auto foo2 = [x, y]()mutable { ++x, ++y; };
-    //std::cout << x << " " << y << std::endl;加了mutable关键字使得x,y可以修改但是输出结果依旧为1,1是因为值捕获是把原变量拷贝了一份 
+    //std::cout << x << " " << y << std::endl;加了mutable关键字使得x,y可以修改但是输出结果依旧为1,1是因为值捕获是把原变量拷贝了一份
 
-    auto foo3 = [&x, &y] { ++x, ++y; };//引用捕获 
+    auto foo3 = [&x, &y] { ++x, ++y; };//引用捕获
 
     struct A
     {
@@ -27,11 +27,21 @@ int main()
         void test()
         {
             auto foo = [this] { print(); };//this捕获
-            //lambda 表达式通过this捕获使得可以在lambda表达式里直接访问成员 
+            //lambda 表达式通过this捕获使得可以在lambda表达式里直接访问成员
         }
     };
 
     auto foo4 = [=] {};//捕获lambda表达式定义域的全部变量的值
     auto foo5 = [&] {};//捕获lambda表达式定义域的全部变量的引用
+
+    auto foo6 = [i = 2] { std::cout << i; };
+
+    foo6();
+
+    auto foo7 = [&i = x] { ++i; };
+
+    foo7();
+
+    std::cout << x;//2
 }
 
