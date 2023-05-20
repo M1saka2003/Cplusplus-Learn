2a36f9a8d0a3d55ed9336de92cede000cdd3d9d0
M1saka2003 <3296582213@qq.com>
2023-05-20 19:47:25 +0800

    2023/5/20 19:47

--- 测试代码/cmake-build-debug/cmake_install.cmake
+++ 测试代码/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Code/CLion/CPP
+# Install script for directory: D:/Code/CLion/Cplusplus-Learn/测试代码
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CPP")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Cplusplus-Learn")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,11 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/JetBrains/Toolbox/apps/apps/CLion/ch-0/231.9011.31/bin/mingw/bin/objdump.exe")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Code/CLion/CPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Code/CLion/Cplusplus-Learn/测试代码/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- 测试代码/main.cpp
+++ 测试代码/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
-struct B {};
-
-namespace A {
-    struct B {};
-    void foo(B) {
-        std::cout << "foo()\n";
-    }
-}
+struct Mystruct {
+    static std::string msg;
+};
+std::string Mystruct::msg = "OK";
 
 int main() {
-    ::B x;
-    foo(x);
-    A::B y;
-    foo(y);
+
 }
