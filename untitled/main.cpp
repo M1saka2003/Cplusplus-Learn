022e372e39802d2043964a2623b07f532fa8905b
M1saka2003 <3296582213@qq.com>
2023-06-02 15:23:22 +0800

    2023/6/2 15:23

--- untitled/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ranges>
-#include <vector>
-#include <algorithm>
-
-int main() {
-    std::vector vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::sort(vec.begin(), vec.end());
-}
